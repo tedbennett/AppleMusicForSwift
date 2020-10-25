bc494b7391acce646ebcee7b9f5495f12b748b5e
Ted <teddsbennett@gmail.com>
2020-10-25 17:56:16 +0000

    First commit - add initialiser

--- Sources/AppleMusicAPI/AppleMusicAPI.swift
+++ Sources/AppleMusicAPI/AppleMusicAPI.swift
@@ -1,3 +1,17 @@
-struct AppleMusicAPI {
-    var text = "Hello, World!"
+import Foundation
+
+public class AppleMusicAPI {
+    public static let manager = AppleMusicAPI()
+    
+    private init() {}
+    
+    private var developerToken: String?
+    private var userToken: String?
+    private var storefront: String?
+    
+    public func initialize(developerToken: String, userToken: String, storefront: String) {
+        self.developerToken = developerToken
+        self.userToken = userToken
+        self.storefront = storefront
+    }
 }
